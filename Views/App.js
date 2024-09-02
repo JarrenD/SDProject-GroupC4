5539a818b4d92a0d891eca9abbe21a9fa57da03e
dumaMandimo <2548972@students.wits.ac.za>
2024-09-02 12:02:13 +0200

    Change framework to react

---
+++ Views/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NotificationCenter from './notifications';
+
+function App() {
+  return (
+    <div className="App">
+      <NotificationCenter />
+    </div>
+  );
+}
+
+export default App;
