3b6c30ed6ac03972fdfc15be4e3b35acb5ff3fa0
“LupumloKolisi” <2550646@students.wits.ac.za>
2024-09-03 16:21:01 +0200

    added the Navbar

--- Views/src/App.jsx
+++ Views/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import DashboardContent from '../components/DashboardContent';
 import './App.css';
@@ -7,6 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="app-container">
+      <Navbar />
       <Sidebar />
       <div className="main-content">
         <Header />
