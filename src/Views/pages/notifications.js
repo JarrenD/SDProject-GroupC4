3a1f62eefd1a0f850d04aea5c0fb39f719646813
dumaMandimo <2548972@students.wits.ac.za>
2024-09-12 17:47:42 +0200

    Notifications UI

--- src/Views/pages/notifications.js
+++ src/Views/pages/notifications.js
@@ -1,6 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './notifications.css';
-import { fetchAlertsFromFirebase, removeFirebaseListener } from '../../models/firebase/firebaseConfig.js'; 
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBEbqPXRCr6BcsTBoM6VKiHcAFVVkqSW7E",
+    authDomain: "creativetutorial-ba1bf.firebaseapp.com",
+    databaseURL: "https://creativetutorial-ba1bf-default-rtdb.firebaseio.com",
+    projectId: "creativetutorial-ba1bf",
+    storageBucket: "creativetutorial-ba1bf",
+    messagingSenderId: "945665449612",
+    appId: "1:945665449612:web:7cb4ac69350bfc6ad065a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const storage = getStorage(app);
 
 const NotificationCenter = () => {
   const [activeTab, setActiveTab] = useState('inbox');
