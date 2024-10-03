4c6ebb19e8d1316e9702dbf5d7d1a33ef13a24f2
2541773-wits <133588025+2541773-wits@users.noreply.github.com>
2024-10-03 17:21:08 +0200

    Deleted unused imports

--- src/views/components/AdminDashboardContent.jsx
+++ src/views/components/AdminDashboardContent.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import AdminIncidentManagement from './AdminIncidentManagement';
-import AdminEmergencyAlertsPanel from './AdminEmergencyAlertsPanel';
-import AdminUserRoleManagement from './AdminUserRoleManagement';
-import AdminSafetyResources from './AdminSafetyResources';
-import AdminRealTimeNotifications from './AdminRealTimeNotifications';
-import AdminAnalyticsOverview from './AdminAnalyticsOverview';
+
 
 
 const AdminDashboardContent = () => {
